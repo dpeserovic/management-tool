f3e82ed78492df750cd58524abf54d91c7e05e90
dpeserovic <d.peserovic@gmail.com>
2021-03-28 16:39:01 +0200

    add express, mysql and cors dependencies

---
+++ src/client/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'mobx-react';
+import { HistoryAdapter } from 'mobx-state-router';
+import { RootStore } from './common/stores';
+import { history } from './common/router';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function startApp() {
+  const rootStore = new RootStore();
+  const historyAdapter = new HistoryAdapter(rootStore.routerStore.router, history);
+  historyAdapter.observeRouterStateChanges();
+  renderApp(rootStore);
+}
+
+function renderApp(rootStore) {
+  ReactDOM.render(<Provider rootStore={rootStore}><App /></Provider>, document.getElementById('root'));
+}
+
+startApp();
